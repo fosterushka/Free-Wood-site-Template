f32628cbef0b3227d60e8994b0224409d7909a65
AMR <man1080@yandex.ru>
2019-07-23 02:22:07 +0400

    reDesign

--- index.php
+++ index.php
@@ -4,127 +4,43 @@
     <meta charset="UTF-8">
     <title>Купить дрова с доставкой Ижевск</title>
     <link href="https://fonts.googleapis.com/css?family=Play&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/iconmonstr-iconic-font.css">
 </head>
 <body>
 
-<header>
-    <div class="row-h">
-        <div class="header">
-            <p style="font-size: 38px;" class="txt-ylw">Дрова Ижевск</p>
-            <p style="font-size: 24px; text-align: right; color: white;">+7 950 819 08 87<br><span class="txt-red">Сделайте заказ прямо сейчас</span>
-            </p>
+<section id="sectionOne">
+        <h1>ДРОВА КОЛОТЫЕ ИЖЕВСК</h1>
+        <p>БЕРЁЗОВЫЕ, ДУБОВЫЕ, ОЛЬХОВЫЕ, ОСИНОВЫЕ И ЕЛОВЫЕ ДРОВА С ДОСТАВКОЙ</p>
+</section>
+
+<section id="sectionTwo">
+    <h2 class="section-title">Наши услуги</h2>
+    <div class="itemBox">
+        <div class="item">
+            <div class="itemImg"></div>
+            <div class="itemDescription">
+
+            </div>
         </div>
     </div>
+</section>
+
+<section id="sectionThree">
+
+</section>
+
+<header>
+        <div class="logo"></div>
+        <div class="header-link">
+            <a href="#sectionOne">Главная</a>
+            <a href="#sectionTwo">Цены</a>
+            <a href="#sectionThree">Контакты</a>
+            <a href="tel:+7 950 819 08 87">+7 950 819 08 87</a>
+        </div>
 </header>
 
-<section class="section-one">
-    <div class="overlayBlack"></div>
-    <div class="row-h">
-        <div class="section-one-intro">
-            <div class="section-one-info">
-                <p>ПРОДАЖА ДРОВ С ДОСТАВКОЙ</p><br>
-                <p>В ИЖЕВСКЕ</p>
-            </div>
-            <div class="section-one-img">
-                <img src="/assets/toporinLog.png" width="300" alt="">
-            </div>
-        </div>
-    </div>
-    <a href="#section3" class="section-one-btn">Посмотреть вид услуг</a>
-</section>
-
-<section id="section2">
-    <div class="overlayBlack"></div>
-    <video autoplay muted loop id="myVideo">
-        <source src="assets/bg1.mp4" type="video/mp4">
-    </video>
-    <div class="row-h">
-        <div class="section-two">
-            <div class="section-two-item">
-                <div class="section-two-item-title">Качество древесины,<br> и честный объем </div>
-                <div class="section-two-item-img"><img src="/assets/w1.png" alt=""></div>
-            </div>
-            <div class="section-two-item">
-                <div class="section-two-item-title">Бесплатная доставка</div>
-                <div class="section-two-item-img"><img src="/assets/w2.png" alt=""></div>
-            </div>
-            <div class="section-two-item">
-                <div class="section-two-item-title">Доступные цены </div>
-                <div class="section-two-item-img"><img src="/assets/w3.png" alt=""></div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="section3">
-    <div class="row-h">
-        <div class="section-three">
-            <h2>НАШИ УСЛУГИ</h2>
-            <div class="service-box">
-                <div class="service-box-item">
-                    <div class="hotSale">ХИТ ПРОДАЖ</div>
-                    <img src="/assets/mix1.png" width="300" alt="">
-                    <div class="item-title">ЛИПА / ОСИНА / ЕЛЬ</div>
-                    <p> 1,2куб Пикап - 2000 тыс.р</p>
-                    <p>2,5куб Газель - 4000 тыс.р</p>
-                    <p>5куб Газ Самосвал - 7500 тыс.р</p>
-                </div>
-                <div class="service-box-item">
-                    <img src="/assets/d1.jpg" alt="">
-                    <div class="item-title">БЕРЕЗА</div>
-                    <p> 1,2куб Пикап - 2300 тыс.р</p>
-                    <p>2,5куб Газель - 4300 тыс.р</p>
-                    <p>5куб Газ Самосвал - 7700 тыс.р</p>
-                </div>
-                <div class="service-box-item">
-                    <img src="/assets/2.png" width="300" alt="">
-                    <div class="item-title">СУХАЯ БЕРЕЗА</div>
-                    <p> 1,2куб Пикап - 4300 тыс тыс.р</p>
-                    <p>2,5куб Газель - 7300 тыс.р</p>
-                    <p>5куб Газ Самосвал - 14500 тыс.р</p>
-                </div>
-                <div class="service-box-item">
-                    <img src="/assets/d3.png" alt="">
-                    <div class="item-title">ЕЛЬ СУХАЯ</div>
-                    <p> 1,2куб Пикап - 2300 тыс.р</p>
-                    <p>2,5куб Газель - 4300 тыс.р</p>
-                    <p>5куб Газ Самосвал - 7500 тыс.р</p>
-                </div>
-                <div class="service-box-item">
-                    <img src="/assets/mixItem.png" alt="">
-                    <div class="item-title">СМЕШАННАЯ ОСИНА,ЛИПА И ЕЛЬ</div>
-                    <p> 1,2куб Пикап - 2300 тыс.р</p>
-                    <p>2,5куб Газель - 4000 тыс.р</p>
-                    <p>5куб Газ Самосвал - 7500 тыс.р</p>
-                </div>
-                <div class="service-box-item">
-                    <img src="/assets/oak.png" width="200" alt="">
-                    <div class="item-title">СМЕШАННАЯ СУХАЯ И СЫРАЯ БЕРЕЗА <br>50% / 50%</div>
-                    <p> 1,2куб Пикап - 3000 тыс.р</p>
-                    <p>2,5куб Газель - 5300 тыс.р</p>
-                    <p>5куб Газ Самосвал - 9700 тыс.р</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-</section>
-
-<section id="section4">
-    <div class="overlayBlack"></div>
-    <h2 class="z-indexUP">КОНТАКТЫ</h2>
-    <div class="section4-block-info z-indexUP">
-        <a href=""><i class="im im-whatsapp"></i></a>
-        <a href=""><i class="im im-mail"></i></a>
-        <a href=""><i class="im im-phone"></i></a>
-        <a href=""><i class="im im-paperplane"></i></a>
-    </div>
-</section>
-
-
-<script src="/assets/jquery.js"></script>
-<script src="/assets/scripts.js"></script>
+<script src="/assets/js/jquery.js"></script>
+<script src="/assets/js/scripts.js"></script>
 </body>
 </html>
